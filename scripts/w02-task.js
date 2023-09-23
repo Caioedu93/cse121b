854efb62d1321d3cdf96a98abaaf260a600124b8
Caioedu93 <depaulacaio10@gmail.com>
2023-09-23 20:33:09 -0300

    w02-task

---
+++ scripts/w02-task.js
@@ -0,0 +1,45 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+const fullName = 'Caio Eduardo Jesus de Paula';
+const currentYear = new Date().getFullYear();
+const profilePicture = 'images/me.jpg'
+
+
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+
+
+
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}<strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src',profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+
+
+
+
+
+
+/* Step 5 - Array */
+const favoriteFoods = ['Pizza', 'Hamburger','roast Beef','Lemon Pie'];
+foodElement.textContent = favoriteFoods.join(', ');
+const newFavoriteFood = 'Lasagna';
+favoriteFoods.push(newFavoriteFood);
+foodElement.textContent = favoriteFoods.join(', ') +',' ;
+favoriteFoods.shift();
+foodElement.textContent += favoriteFoods.join(', ') + ',';
+favoriteFoods.pop();
+foodElement.textContent += favoriteFoods.join(', ');
+
+
